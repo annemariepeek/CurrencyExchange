6b368c17f6361a3c078c3550d22ada99fe7ea797
annemariepeek <annemariepeek@hotmail.com>
2022-12-16 16:00:36 -0500

    add connection Exchange API and exchange.ejs file

--- currencyExchange.js
+++ currencyExchange.js
@@ -37,24 +37,44 @@ app.get("/", (request, response) => {
 });
 
 
-app.post("/exchange", (request, repsponse) => {
+app.post("/exchange", (request, response1) => {
     const { from_curr, to_curr , amount} = request.body;
 
 
-    fetch('https://currency-exchange.p.rapidapi.com/exchange?from=${from_curr}&to=${to_curr}&q=1.0', options)
+    fetch(`https://currency-exchange.p.rapidapi.com/exchange?from=${from_curr}&to=${to_curr}&q=1.0`, options)
         .then(response => response.json())
-        .then(response => 
+        .then(response => {
+            const convertion = amount * response
+
+            const total_convertion = {
+                amount: amount,
+                from_curr: from_curr,
+                to_curr: to_curr,
+                exchange_rate: response,
+                convertion: convertion
+            }
+
+            async function addConvertionToDB() {
+                try {
+                    await client.connect();
+                   
+                    await addConvertion(client, databaseAndCollection, total_convertion);
             
+                } catch (e) {
+                    console.error(e);
+                } finally {
+                    await client.close();
+                }
+            }
+            addConvertionToDB().catch(console.error);
             
-            console.log(response)
-            
-            // const variables = {
-            //     exchange_rate: rawListeners,
-            //     convertion: convertion
-            // }
-            // )
-        .catch(err => console.error(err));
+            response1.render("exchange", variables);
+        }).catch(err => console.error(err));
 });
 
+async function addConvertion(client, databaseAndCollection, newConvertion) {
+    const result = await client.db(databaseAndCollection.db).collection(databaseAndCollection.collection).insertOne(newConvertion);
+}
 
 app.listen(portNumber); 
+console.log(`Webserver started and running at http://localhost:${portNumber}`);
